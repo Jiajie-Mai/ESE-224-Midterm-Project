cdc86d2417ca52d7a06712f880fac8a34178ad42
gibjun <31456953+gibjun@users.noreply.github.com>
2020-10-30 01:46:12 -0400

    Add files via upload

--- ESE 224 Midterm Project/ESE 224 Midterm Project/Student.cpp
+++ ESE 224 Midterm Project/ESE 224 Midterm Project/Student.cpp
@@ -3,13 +3,40 @@
 Student::Student() {
 
 }
+ostream& operator<< (ostream& output, Student& student) { // add to later
+	output << "\tUsername:\t\t" << student.getUsername() << endl;
+	output << "\tPassword:\t\t" << student.getPassword() << endl;
+	output << "\tmaxCopies:\t\t" << student.getMaxCopies() << endl;
+	return output;
+}
+istream& operator>> (istream& input, Student& student) { // add to later
+	string username, password;
+	int maxCopies;
+	input >> username >> password >> maxCopies;	//add more stuff later
+	student.setUsername(username);
+	student.setPassword(password);
+	student.setMaxCopies(maxCopies);
+	return input;
 
-void Student::operator>>(Student input)
-{
-	
 }
 
-void Student::operator<<(Student input)
-{
+string Student::getUsername() {
+	return Username;
+}
+string Student::getPassword() {
+	return Password;
+}
+int Student::getMaxCopies() {
+	return maxCopies;
+}
 
+// mutator functions
+void Student::setUsername(string Username) {
+	this->Username = Username;
+}
+void Student::setPassword(string Password) {
+	this->Password = Password;
+}
+void Student::setMaxCopies(int maxCopies) {
+	this->maxCopies = maxCopies;
 }
